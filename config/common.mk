4d003c8e41f79df3fedd6377dc38e8e5cd2f7eed
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-31 15:15:28 +0000

    vendor: notch-city: Add 3 mode display cutout handler [2/3]
    [@AgentFabulous - POSP]
    - Introduces the HideCutout and StatusBarStock overlay used in the
      3 mode display cutout handler. The HideCutout overlay is necessary
      since we can't register a content observer in the display manager code.
      We only have access to resources during boot. Thus, leave this as an
      overlay and let the config and overlay change methods handle this.
      Though we can probably do statusbar stock height toggling in the
      SystemUI code without overlays, I kinda got lazy by the end, just
      live with it god damn it xD
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I62f63f39bcb410cfbc68e0028b9cef3d748d7eb6

--- config/common.mk
+++ config/common.mk
@@ -173,7 +173,10 @@ PRODUCT_PACKAGES += \
 include vendor/aosp/config/branding.mk
 
 # OTA
-include vendor/aosp/config/ota.mk
+#include vendor/aosp/config/ota.mk
+
+#Packages
+include vendor/aosp/config/packages.mk
 
 # GApps
 include vendor/gapps/config.mk
